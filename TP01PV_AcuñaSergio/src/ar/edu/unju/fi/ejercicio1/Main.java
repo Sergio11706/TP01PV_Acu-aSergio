c92a0240c01a7c6717bc94120fc3971a12832470
sergi <sergi@LAPTOP-K2ASHF92>
2024-04-09 18:55:31 -0300

    Ejercicio 1

---
+++ TP01PV_AcuñaSergio/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n1=4,n2=7,n3=9,n4=3,n5=6;
+		float promedio = (float) (n1+n2+n3+n4+n5)/5;
+		System.out.println(n1+","+n2+","+n3+","+n4+","+n5);
+		System.out.println("El promedio es: "+promedio);
+	}
+
+}
