0b636db156d556ee76c1d576a678013ea63e186f
sergi <sergi@LAPTOP-K2ASHF92>
2024-04-09 22:35:41 -0300

    Ejercicio 2

---
+++ TP01PV_AcuñaSergio/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nombre_pais = "Alemania";
+		int edad = 13;
+		float altura_edif = 80.75f;
+		float precio_prod = 3.99f;
+		String num_telf = "388091218";
+		double cos = Math.cos(0.5);
+		
+		System.out.println("Nombre de un Pais: "+nombre_pais);
+		System.out.println("Edad: "+edad);
+		System.out.println("Altura de un edificio: "+altura_edif);
+		System.out.println("Precio de un producto: $"+precio_prod);
+		System.out.println("Numero de Telefono: "+num_telf);
+		System.out.println("Coseno de 0.5: "+cos);
+	}
+
+}
